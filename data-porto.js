3e8ba63a35f19b420baf6f9485d42e24264d5f76
faiisll <faisal.ayas72@gmail.com>
2021-08-11 01:16:01 +0700

    first commit

---
+++ data-porto.js
@@ -0,0 +1,47 @@
+let porto = [
+
+    {
+        title: "Doa Q - Moslem pray handbook",
+        desc: "This application is made for final year project in my degree. Doa Q is handbook for moslem to pray. I made this App with Laravel as backend and React Native for the frontend.",
+        img: "porto5.png",
+        live: "https://play.google.com/store/apps/details?id=com.doaku",
+        source: "http://doaq.herokuapp.com/",
+        tech: ["laravel", "react", "js"]
+    },
+    {
+        title: "Social Media Dashboard",
+        desc: "You can get this project from Frotendmentor.io. It was challenging for me to convert design to real web page. I used css grid & flexbox to make the structure of social media data card. This project only has one functionality, which is theme switcher. The functionality was made with vanilla js.",
+        img: "porto1.jpg",
+        live: "https://frontend-mentor-social-media-dashboard-sable.vercel.app/",
+        source: "https://github.com/faiisll/frontend-mentor-todo-app-main",
+        tech: ["html", "css", "js"]
+    },
+
+    {
+        title: "Todo List App",
+        desc: "Todo list app made by html, css, and vanilla js, this app has few features like, add, remove, filter the tasks. This project also provided by Frontendmentor.io",
+        img: "porto2.jpg",
+        live: "https://frontend-mentor-todo-app-main.vercel.app/",
+        source: "https://github.com/faiisll/frontend-mentor-Social-media-dashboard-with-theme-switcher",
+        tech: ["html", "css", "js"]
+    },
+
+    {
+        title: "Country Data App",
+        desc: "This Project based on react js, and i used styled-component to styling in this app. This challenge also provide by frontendmentor.io. This app has features like searching country data, filtering, and see the details of the country. The data provided by restcountries.eu via REST API.",
+        img: "porto3.jpg",
+        live: "https://frontend-mentor-country-data-app.vercel.app/",
+        source: "https://github.com/faiisll/frontend-mentor-country-data-app",
+        tech: ["html", "css", "js", "react"]
+    },
+
+    {
+        title: "IP Tracker App",
+        desc: "This Project based on react js, and i used styled-component to styling in this app. This challenge also provide by frontendmentor.io. This app has features like searching country data, filtering, and see the details of the country. The data provided by restcountries.eu via REST API.",
+        img: "porto4.jpg",
+        live: "https://frontend-mentor-iptracker.vercel.app/",
+        source: "https://github.com/faiisll/frontend-mentor-iptracker",
+        tech: ["html", "css", "js", "react"]
+    },
+
+]
---
+++ script.js
@@ -0,0 +1,82 @@
+let navMenu = document.querySelectorAll(".nav-item");
+let section = document.querySelectorAll(".section");
+function resetActiveNav(){
+  for(var i = 0; i < navMenu.length; i++){
+    navMenu[i].classList.remove("active");
+  }
+}
+
+
+function scrollSpy(position){
+  let hero = document.getElementById("home");
+  let works = document.getElementById("works");
+
+  
+  for(var i = 0; i < section.length; i++){
+    if(section[i].scrollTop <= position){
+      resetActiveNav();
+      section[i].classList.add("active");
+
+    }
+  }
+}
+
+let slider = new Splide(".splide", {
+  gap: 2,
+});
+
+const portos = document.getElementById("portos");
+let html = "";
+porto.map( p => {
+  html += `
+  <li class="splide__slide">
+  <div class="splide__slide__container">
+    <div class="porto-slider">
+      <img
+        src="assets/porto/${p.img}"
+        alt=""
+        class="porto-slider-img"
+      />
+      <div class="porto-slider-description">
+        <h1 class="porto-slider-h1">${p.title}</h1>
+        <div class="porto-slider-tech">
+          ${p.tech.map( (t) => `<img src="assets/tech/${t}.png" width="32px" alt="${t}" />`).join("")}
+          
+        </div>
+        <p>${p.desc}</p>
+        <div class="porto-slider-footer">
+          <a href="${p.live}" class="btn btn-primary">Live</a>
+          <a href="${p.source}" class="btn btn-light">Source Code</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</li>
+  `;
+});
+
+portos.innerHTML = html;
+
+slider.mount();
+AOS.init();
+
+
+let navbar = document.getElementsByClassName("navbar")[0];
+navbar.style.boxShadow = 
+  window.scrollY > 100
+    ? "0px 4px 16px -6px rgba(189,189,189,0.78)"
+    : "0px 4px 16px -200px rgba(189,189,189,0.78";
+
+window.addEventListener("scroll", function (e) {
+  var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  if (this.scrollY > 100) {
+    navbar.style.backgroundColor = "rgba(255,255,255, 1)";
+    navbar.style.boxShadow = "0px 4px 16px -6px rgba(189,189,189,0.78)";
+  } else {
+    navbar.style.backgroundColor = "rgba(255,255,255, 0)";
+    navbar.style.boxShadow = "0px 4px 16px -200px rgba(189,189,189,0.78";
+  }
+
+  scrollSpy(scrollPosition);
+
+});
